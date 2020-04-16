6109aa48a30ab4c74d2f3777e9be0ab160c85c5c
Edward Meehan <edward.meehan.808@gmail.com>
2020-04-15 23:48:43 -0700

    first commit - new project

---
+++ src/client/components/about-sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const AboutPage = () => (
+  <div className="sheetNameText">
+    <div>Github repo:</div>
+    <a
+      href="https://www.github.com/enuchi/React-Google-Apps-Script"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      React + Google Apps Script
+    </a>
+    <div>-Elisha Nuchi</div>
+  </div>
+);
---
+++ src/client/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SheetEditor from './components/sheet-editor';
+import './styles.css';
+
+
+ReactDOM.render(
+  <SheetEditor />,
+  document.getElementById('index')
+);
